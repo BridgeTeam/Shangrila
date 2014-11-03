d44b9f57431d6892298308a018e5484be08d062e
Dragon <ismail.rebah@gmail.com>
2014-11-03 13:56:28 +1300

    añadidos filtro precio

--- js/cierre.js
+++ js/cierre.js
@@ -1,15 +1,21 @@
-var cierreApp = angular.module('cierreHoy', []);
+var cierreModule = angular.module('cierreHoy', []);
 
-cierreApp.controller('CierreController', function($scope, $http){
+cierreModule.controller('CierreController', function($scope, $http){
 	$http.get('../librerias/cierre.php').success(function(data){
 		$scope.cierre=data;
 		
-		$scope.cierre.valorTotal=0.00;		
+		$scope.cierre.valorTotal=0.00;
 		$.each($scope.cierre.valoresCierre, function(i, item){
 			$scope.cierre.valorTotal+=item.valor;
-			$scope.cierre.valoresCierre[i].valor=(item.valor.toFixed(2)).toString().replace('.',',');
 		});
-		$scope.cierre.valorTotal=($scope.cierre.valorTotal.toFixed(2)).toString().replace('.',',');
+		$scope.cierre.valorTotal=$scope.cierre.valorTotal;
 	});
 	
+});
+
+cierreModule.filter('precioComa2', function(){
+	var preciosComa2Filter = function(input){
+		return (input.toFixed(2)).toString().replace('.',',') + " €";
+	};
+	return preciosComa2Filter;
 });
